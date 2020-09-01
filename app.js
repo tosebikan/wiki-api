e41fba6ac29a3cba354b250486c76b1b70f87034
Olaosebikan Tosin Tolu <tosebikan@gmail.com>
2020-09-01 21:39:08 +0000

    add backend

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const app = express();
+
+app.set("view engine", ejs);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+const articlesSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
+
+const Article = mongoose.model("Article", articlesSchema);
+
+app.get("/articles", (req, res) => {
+  Article.find({}, (err, article) => {
+    if (!err) {
+      res.send(article);
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.post("/articles", (req, res) => {
+  const title = req.body.title;
+  const content = req.body.content;
+
+  const newArticle = new Article({
+    title: title,
+    content: content
+  });
+
+  newArticle.save(err => {
+    if (!err) {
+      res.send("succesfully added a new article");
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log("server running on port 3000");
+});
